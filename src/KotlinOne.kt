483c407f2cd0f7607bf8206246c1acfc8404a0cc
MwauraLenny <mwangileo1@gamil.com>
2024-03-18 16:35:56 +0300

    first commit

---
+++ src/KotlinOne.kt
@@ -0,0 +1,89 @@
+fun main(args: Array<String>) {
+    println("This is my first Kotlin file")
+    println("My name is Lenny")
+    println("I am a Software Developer")
+    println(434)
+    println(34)
+    //variables and data types
+    var a: Int
+    a = 78
+    var b: String
+    b ="This is me"
+    var c: Double
+    c = 99.2
+    var d:Boolean
+    d = true
+    var e:Char
+    e = 'a'
+
+    println("I got $c marks in my English class")
+    println("my grandad is $a years old")
+       //2
+    var f: String ="How are you?"
+    var yearofbirth: Int=2000
+
+    println(f)
+    println(yearofbirth)
+
+    //3
+
+    var name ="Lenny"
+    var age = 20
+
+    println(name)
+    println(age)
+
+
+    val firstName = "Lenny"
+    val lastname = "Mwaura"
+    println(firstName)
+    println(lastname)
+
+    var FirstNamen="Peter"
+    var LastNamen="Ngugi"
+    var agen = 34
+    var Locationn = "Kisumu"
+
+    println("$LastNamen $FirstNamen is $agen years old and lives in $Locationn")
+
+    val FirstNumber =23
+    val SecondNumber =56
+    val ThirdNumber =45.6
+    val FourthNumber =12.3
+//
+//    var  summation = FirstNumber + SecondNumber
+//    println(summation)
+//    println(FirstNumber+SecondNumber)
+//
+//    var subtraction =ThirdNumber -FirstNumber
+//    println(subtraction)
+//
+//    var multiplication =FourthNumber*SecondNumber
+//    println(multiplication)
+//
+//    var division =SecondNumber/FourthNumber
+//    println(division)
+//
+//    var modulas =ThirdNumber%SecondNumber
+//    println(modulas)
+
+
+    var x =20.3
+    var y=37.5
+    var z=49.2
+
+    var summation= z+x
+    var Subtraction = z-y
+    var multiplication = x*z
+    var division = z/x
+
+    println(summation)
+    println(Subtraction)
+    println(multiplication)
+    println(division)
+
+
+
+
+
+}
